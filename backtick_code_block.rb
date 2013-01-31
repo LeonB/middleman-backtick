e0ecd9d0d508c8f6d1350b459e798870fedce741
Leon Bogaert <leon@tim-online.nl>
2013-01-31 22:35:16 +0100

    GD indention

--- backtick_code_block.rb
+++ backtick_code_block.rb
@@ -1,66 +1,66 @@
 module BacktickCodeBlock
   class << self
-		AllOptions = /([^\s]+)\s+(.+?)(https?:\/\/\S+)\s*(.+)?/i
-		LangCaption = /([^\s]+)\s*(.+)?/i
+    AllOptions = /([^\s]+)\s+(.+?)(https?:\/\/\S+)\s*(.+)?/i
+    LangCaption = /([^\s]+)\s*(.+)?/i
 
     def registered(app)
-    	# print app.methods
+      # print app.methods
       app.before_render do |content, renderer|
-		require 'pp'
-		pp app
-		print Middleman::CoreExtensions::ExternalHelpers.inspect
+    require 'pp'
+    pp app
+    print Middleman::CoreExtensions::ExternalHelpers.inspect
         # replacement = BacktickCodeBlock.render_code_block(content)
         # content.replace(replacement)
         # content.sub!('Ubuntu', 'Ubuntu LOL !!!!')
       end
     end
 
-	  def render_code_block(input)
-	    @options = nil
-	    @caption = nil
-	    @lang = nil
-	    @url = nil
-	    @title = nil
-	    input.gsub(/^`{3} *([^\n]+)?\n(.+?)\n`{3}/m) do
-	      @options = $1 || ''
-	      str = $2
+    def render_code_block(input)
+      @options = nil
+      @caption = nil
+      @lang = nil
+      @url = nil
+      @title = nil
+      input.gsub(/^`{3} *([^\n]+)?\n(.+?)\n`{3}/m) do
+        @options = $1 || ''
+        str = $2
 
-	      if @options =~ AllOptions
-	        @lang = $1
-	        @caption = "<figcaption><span>#{$2}</span><a href='#{$3}'>#{$4 || 'link'}</a></figcaption>"
-	      elsif @options =~ LangCaption
-	        @lang = $1
-	        @caption = "<figcaption><span>#{$2}</span></figcaption>"
-	      end
+        if @options =~ AllOptions
+          @lang = $1
+          @caption = "<figcaption><span>#{$2}</span><a href='#{$3}'>#{$4 || 'link'}</a></figcaption>"
+        elsif @options =~ LangCaption
+          @lang = $1
+          @caption = "<figcaption><span>#{$2}</span></figcaption>"
+        end
 
-	      if str.match(/\A( {4}|\t)/)
-	        str = str.gsub(/^( {4}|\t)/, '')
-	      end
-	      if @lang.nil? || @lang == 'plain'
-	        code = tableize_code(str.gsub('<','&lt;').gsub('>','&gt;'))
-	        "<figure class='code'>#{@caption}#{code}</figure>"
-	      else
-	        if @lang.include? "-raw"
-	          raw = "``` #{@options.sub('-raw', '')}\n"
-	          raw += str
-	          raw += "\n```\n"
-	        else
-	          code = self.block_code(str, @lang)
-	          "<figure class='code'>#{@caption}#{code}</figure>"
-	        end
-	      end
-	    end
-	  end
+        if str.match(/\A( {4}|\t)/)
+          str = str.gsub(/^( {4}|\t)/, '')
+        end
+        if @lang.nil? || @lang == 'plain'
+          code = tableize_code(str.gsub('<','&lt;').gsub('>','&gt;'))
+          "<figure class='code'>#{@caption}#{code}</figure>"
+        else
+          if @lang.include? "-raw"
+            raw = "``` #{@options.sub('-raw', '')}\n"
+            raw += str
+            raw += "\n```\n"
+          else
+            code = self.block_code(str, @lang)
+            "<figure class='code'>#{@caption}#{code}</figure>"
+          end
+        end
+      end
+    end
 
-	  def tableize_code(str, lang = '')
-	    table = '<div class="highlight"><table><tr><td class="gutter"><pre class="line-numbers">'
-	    code = ''
-	    str.lines.each_with_index do |line,index|
-	      table += "<span class='line-number'>#{index+1}</span>\n"
-	      code  += "<span class='line'>#{line}</span>"
-	    end
-	    table += "</pre></td><td class='code'><pre><code class='#{lang}'>#{code}</code></pre></td></tr></table></div>"
-	  end
+    def tableize_code(str, lang = '')
+      table = '<div class="highlight"><table><tr><td class="gutter"><pre class="line-numbers">'
+      code = ''
+      str.lines.each_with_index do |line,index|
+        table += "<span class='line-number'>#{index+1}</span>\n"
+        code  += "<span class='line'>#{line}</span>"
+      end
+      table += "</pre></td><td class='code'><pre><code class='#{lang}'>#{code}</code></pre></td></tr></table></div>"
+    end
 
     alias :included :registered
   end
