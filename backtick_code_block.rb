a05f3f07a5004ae6faad08f9db8eaa04a83923ed
Leon Bogaert <leon@tim-online.nl>
2013-02-13 22:20:36 +0100

    Mixing the render_code_block_method Instead of calling it as a static method

--- backtick_code_block.rb
+++ backtick_code_block.rb
@@ -14,48 +14,48 @@ module BacktickCodeBlock
           next
         end
 
-        BacktickCodeBlock.render_code_block(body)
+        render_code_block(body)
       end
-    end
 
-    def render_code_block(input)
-      @options = nil
-      @caption = nil
-      @lang = nil
-      @url = nil
-      @title = nil
-      input.gsub(/^`{3} *([^\n]+)?\n(.+?)\n`{3}/m) do
-        @options = $1 || ''
-        str = $2
+      def render_code_block(input)
+        @options = nil
+        @caption = nil
+        @lang = nil
+        @url = nil
+        @title = nil
+        input.gsub(/^`{3} *([^\n]+)?\n(.+?)\n`{3}/m) do
+          @options = $1 || ''
+          str = $2
 
-        if @options =~ AllOptions
-          @lang = $1
-          @caption = "<figcaption><span>#{$2}</span><a href='#{$3}'>#{$4 || 'link'}</a></figcaption>"
-        elsif @options =~ LangCaption
-          @lang = $1
-          @caption = "<figcaption><span>#{$2}</span></figcaption>" if $2
-        end
+          if @options =~ AllOptions
+            @lang = $1
+            @caption = "<figcaption><span>#{$2}</span><a href='#{$3}'>#{$4 || 'link'}</a></figcaption>"
+          elsif @options =~ LangCaption
+            @lang = $1
+            @caption = "<figcaption><span>#{$2}</span></figcaption>" if $2
+          end
 
-        if str.match(/\A( {4}|\t)/)
-          str = str.gsub(/^( {4}|\t)/, '')
-        end
+          if str.match(/\A( {4}|\t)/)
+            str = str.gsub(/^( {4}|\t)/, '')
+          end
 
-        if @lang.nil? || @lang == 'plain' || @lang == ''
-          @lang = 'text'
-        end
+          if @lang.nil? || @lang == 'plain' || @lang == ''
+            @lang = 'text'
+          end
 
-        if @lang.include? "-raw"
-          raw = "``` #{@options.sub('-raw', '')}\n"
-          raw += str
-          raw += "\n```\n"
-        else
-          code = self.block_code(str, @lang)
-          "<figure class='code'>
-            #{@caption}
-            <div>
-              #{code}
-            </div>
-          </figure>"
+          if @lang.include? "-raw"
+            raw = "``` #{@options.sub('-raw', '')}\n"
+            raw += str
+            raw += "\n```\n"
+          else
+            code = self.block_code(str, @lang)
+            "<figure class='code'>
+              #{@caption}
+              <div>
+                #{code}
+              </div>
+            </figure>"
+          end
         end
       end
     end
